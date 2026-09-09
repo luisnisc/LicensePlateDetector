21dd74ddee84e0f5868fd752d806540fbf9b81b7
luisnisc <luisnisc30mayo2005@gmail.com>
2026-09-09 07:09:37 +0200

    Frontend HTTP Token

--- web-panel/src/App.jsx
+++ web-panel/src/App.jsx
@@ -9,6 +9,28 @@ import { WhitelistTable } from './components/WhitelistTable';
 import { ActivityLogs } from './components/ActivityLogs';
 import './App.css';
 
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('jwt_token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+      localStorage.removeItem('jwt_token');
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
+
 function App() {
   const { whitelist, logs, API_URL } = useLprData();
 
